64f96674645e5aae997abafabb5c1819a2a6b72b
kochezinha <163464664+kochezinha@users.noreply.github.com>
2024-09-19 11:25:12 -0300

    arquivos do projeto

---
+++ footer.php
@@ -0,0 +1,11 @@
+            </div>
+        </div>
+    </div>
+
+    <!-- Rodapé do Sistema -->
+    <div class="mt-5 bg-success text-white text-center fixed-bottom">
+        <p>LabTec - Seu laboratório sempre funcionando, para você focar no que importa!</p>
+    </div>
+
+</body>
+</html>
